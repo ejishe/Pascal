d2b3a0604094c7e59867b1461cf9bf3f84de7f10
ejishe <162468377+ejishe@users.noreply.github.com>
2024-04-17 07:15:21 +0300

    Add files via upload

---
+++ HexMath.pas
@@ -0,0 +1,57 @@
+﻿Unit HexMath; 
+interface 
+function HexCalc(number1,sign,number2:string):string;
+function hextoint(number: string): biginteger;
+function IntToHex(number:biginteger):string;  
+
+
+implementation  
+
+function IntToHex(number:biginteger):string; 
+  begin 
+  var hex:='0123456789ABCDEF'; 
+  while number >0 do 
+    begin 
+      result:= hex[integer((number mod 16) +1)] + result; 
+      number:= number div 16;
+    end; 
+end;  
+
+function hextoint(number: string): biginteger;
+var
+  hex := '0123456789ABCDEF';
+  power16: bigInteger;
+begin
+  for var i := 1 to Length(number) do
+  begin
+    power16 := 1;
+    for var j := 1 to Length(number) - i do
+      power16 := power16 * 16;
+    result := result + biginteger(pos(number[i], hex) - 1) * power16;
+  end;
+end;
+
+
+function HexCalc(number1,sign,number2:string):string; 
+begin 
+var a:biginteger;
+a:=1;
+var b:=0;
+case sign of 
+'+': result := IntToHex(HexToInt(number1) + HexToInt(number2)); 
+'-': result := IntToHex(HexToInt(number1) - HexToInt(number2)); 
+'*': result := IntToHex(HexToInt(number1) * HexToInt(number2)); 
+'/': result := IntToHex((HexToInt(number1) / HexToInt(number2))); 
+'**': 
+  begin
+    while b<>HexToInt(number2) do
+      begin
+      a := a*(HexToInt(number1));
+      b+=1;
+      end;
+      result := inttohex(a);
+  end; 
+end; 
+
+end;
+end.
---
+++ hex2.pas
@@ -0,0 +1,37 @@
+﻿ Unit hex2; 
+interface 
+function addition(hex1,hex2:string):string;
+
+
+implementation  
+function symtohex(digit: integer): string;
+begin
+  case digit of
+    0..9: Result := Chr(digit + 48);
+    10..15: Result := Chr(digit + 55);
+  end;
+end;
+
+function addition(hex1,hex2:string):string;
+  var
+  vume, sum, d1, d2: integer;
+begin
+    while Length(hex1) < Length(hex2) do
+    hex1 := '0' + hex1;
+  while Length(hex2) < Length(hex1) do
+    hex2 := '0' + hex2;
+     for var i := Length(hex1) downto 1 do
+  begin
+    d1 := Pos(hex1[i], '0123456789ABCDEF') - 1;
+    d2 := Pos(hex2[i], '0123456789ABCDEF') - 1;
+    sum := d1 + d2 + vume;
+    vume := sum div 16;
+    sum := sum mod 16;
+    result := symtohex(sum) + result;
+  end;
+  if vume > 0 then
+    result := symtohex(vume) + result;
+end;
+
+
+end.
